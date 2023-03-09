8a68ffe282619f7e4520557c5222500e7774a2ee
Will Ceolin <wceolin@users.noreply.github.com>
2023-03-09 00:19:58 -0300

    Deps: Update to Phoenix 1.7.1

--- mix.exs
+++ mix.exs
@@ -36,29 +36,29 @@ defmodule Zoonk.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.0-rc.2", override: true},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.0"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.3"},
-      {:heroicons, "~> 0.5"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
-      {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
-      {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"},
-      {:telemetry_metrics, "~> 0.6"},
-      {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
-      {:sobelow, "~> 0.11.1", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.6.7", only: [:dev, :test], runtime: false},
-      {:mix_audit, "~> 2.1.0", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.2.0", only: [:dev, :test], runtime: false},
+      {:ecto_sql, "~> 3.9.2"},
+      {:esbuild, "~> 0.6.1", runtime: Mix.env() == :dev},
+      {:finch, "~> 0.14"},
+      {:floki, ">= 0.34.2", only: :test},
+      {:gettext, "~> 0.22.1"},
+      {:heroicons, "~> 0.5.2"},
+      {:jason, "~> 1.4"},
+      {:mix_audit, "~> 2.1.0", only: [:dev, :test], runtime: false},
+      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix_html, "~> 3.3.1"},
+      {:phoenix_live_dashboard, "~> 0.7.2"},
+      {:phoenix_live_reload, "~> 1.4.1", only: :dev},
+      {:phoenix_live_view, "~> 0.18.16"},
+      {:phoenix, "~> 1.7.1", override: true},
+      {:plug_cowboy, "~> 2.6"},
+      {:postgrex, ">= 0.16.5"},
+      {:sobelow, "~> 0.11.1", only: [:dev, :test], runtime: false},
+      {:swoosh, "~> 1.9.1"},
+      {:tailwind, "~> 0.1.10", runtime: Mix.env() == :dev},
+      {:telemetry_metrics, "~> 0.6.1"},
+      {:telemetry_poller, "~> 1.0"}
     ]
   end
 
@@ -70,11 +70,12 @@ defmodule Zoonk.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup"],
+      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
+      "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
