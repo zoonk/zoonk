6f12730ab8ad06c2dcdbc687dad229c1f4e73707
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-22 15:57:06 -0300

    Playground: Grid adjustments

--- lib/zoonk_dev/ui/home.ex
+++ lib/zoonk_dev/ui/home.ex
@@ -5,18 +5,20 @@ defmodule ZoonkDev.Live.UIHome do
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <.card>
-      <.card_header icon="tabler-palette-filled">
-        <.card_title>UI Playground</.card_title>
-        <.card_description>Test and experiment with UI components</.card_description>
-      </.card_header>
+    <article class="grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3">
+      <.card>
+        <.card_header icon="tabler-palette-filled">
+          <.card_title>UI Playground</.card_title>
+          <.card_description>Test and experiment with UI components</.card_description>
+        </.card_header>
 
-      <.card_content>
-        <.text>
-          You can use this space to test different components and their configurations.
-        </.text>
-      </.card_content>
-    </.card>
+        <.card_content>
+          <.text>
+            You can use this space to test different components and their configurations.
+          </.text>
+        </.card_content>
+      </.card>
+    </article>
     """
   end
 
