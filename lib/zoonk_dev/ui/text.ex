bd7529c0eb4478c6398cb260d44f7717d4472dc3
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-22 19:57:04 -0300

    Playground: Text: Organize components

--- lib/zoonk_dev/ui/text.ex
+++ lib/zoonk_dev/ui/text.ex
@@ -5,25 +5,73 @@ defmodule ZoonkDev.Live.UIText do
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <article class="flex flex-col gap-4">
-      <.text tag="h1" size={:header} variant={:primary}>Primary header</.text>
-      <.text tag="h1" size={:header} variant={:secondary}>Secondary header</.text>
+    <article class="zk-grid">
+      <.card>
+        <.card_header>
+          <.card_title>Text Sizes</.card_title>
+          <.card_description>Text can have multiple sizes.</.card_description>
+        </.card_header>
 
-      <.text tag="h2" size={:title} variant={:primary}>Primary title</.text>
-      <.text tag="h2" size={:title} variant={:secondary}>Secondary title</.text>
+        <.card_content class="flex flex-col gap-4">
+          <.text tag="h1" size={:header}>Header Text</.text>
+          <.text tag="h2" size={:title}>Title Text</.text>
+          <.text tag="h3" size={:subtitle}>Subtitle Text</.text>
+          <.text tag="p" size={:body}>Body Text</.text>
+          <.text tag="span" size={:caption}>Caption Text</.text>
+        </.card_content>
+      </.card>
 
-      <.text tag="h3" size={:subtitle} variant={:primary}>Primary subtitle</.text>
-      <.text tag="h3" size={:subtitle} variant={:secondary}>Secondary subtitle</.text>
+      <.card>
+        <.card_header>
+          <.card_title>Text Variants</.card_title>
+          <.card_description>Text can have different color variants.</.card_description>
+        </.card_header>
 
-      <.text tag="p" size={:body} variant={:primary}>Primary body</.text>
-      <.text tag="p" size={:body} variant={:secondary}>Secondary body</.text>
+        <.card_content class="flex flex-col gap-4">
+          <.text tag="p" size={:body} variant={:primary}>Primary Text - Main content text</.text>
+          <.text tag="p" size={:body} variant={:secondary}>
+            Secondary Text - Supporting content text
+          </.text>
+          <.text tag="p" size={:body} variant={:custom} class="text-zk-primary">
+            Custom Text - With custom color
+          </.text>
+        </.card_content>
+      </.card>
 
-      <.text tag="p" size={:caption} variant={:primary}>Primary caption</.text>
-      <.text tag="span" size={:caption} variant={:secondary}>Secondary caption</.text>
+      <.card>
+        <.card_header>
+          <.card_title>HTML Tags</.card_title>
+          <.card_description>Text can use different HTML tags for semantic markup.</.card_description>
+        </.card_header>
 
-      <.text tag="label" size={:body} variant={:custom} class="text-pink-500">
-        Custom text
-      </.text>
+        <.card_content class="flex flex-col gap-4">
+          <.text tag="h1">Heading 1</.text>
+          <.text tag="h2">Heading 2</.text>
+          <.text tag="p">Paragraph</.text>
+          <.text tag="span">Span</.text>
+          <.text tag="label" for="example">Label</.text>
+        </.card_content>
+      </.card>
+
+      <.card>
+        <.card_header>
+          <.card_title>Custom Styling</.card_title>
+          <.card_description>Text can be customized with additional classes.</.card_description>
+        </.card_header>
+
+        <.card_content class="flex flex-col gap-4">
+          <.text tag="p" class="italic">Italic text</.text>
+          <.text tag="p" class="font-bold">Bold text</.text>
+          <.text tag="p" class="underline">Underlined text</.text>
+          <.text
+            tag="p"
+            variant={:custom}
+            class="bg-zk-primary text-zk-primary-foreground rounded px-4 py-2"
+          >
+            Custom background and padding
+          </.text>
+        </.card_content>
+      </.card>
     </article>
     """
   end
