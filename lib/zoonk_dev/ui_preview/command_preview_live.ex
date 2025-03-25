492d62a7a8a2f205cfc090cd96729dead4170ee5
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-24 21:58:17 -0300

    UIPreview: Add search to all examples

--- lib/zoonk_dev/ui_preview/command_preview_live.ex
+++ lib/zoonk_dev/ui_preview/command_preview_live.ex
@@ -66,23 +66,25 @@ defmodule ZoonkDev.UIPreview.CommandPreviewLive do
       </.card>
 
       <.dialog id="search-dialog">
-        <.command_input placeholder="Type to search..." />
+        <form phx-change="search-docs" phx-submit="search-docs">
+          <.command_input placeholder="Type to search..." />
+        </form>
 
         <.command_list>
-          <.command_item :for={i <- 1..5}>
+          <.command_item :for={item <- @doc_results}>
             <.icon name="tabler-file-text" />
-            <span>Documentation item {i}</span>
+            <span>{item.label}</span>
           </.command_item>
         </.command_list>
       </.dialog>
 
       <.dialog id="settings-dialog">
-        <form phx-change="search" phx-submit="search">
+        <form phx-change="search-settings" phx-submit="search-settings">
           <.command_input placeholder="Search settings..." icon="tabler-settings" />
         </form>
 
         <.command_list>
-          <.command_item :for={item <- @results}>
+          <.command_item :for={item <- @settings_results}>
             <.icon name={item.icon} />
             <span>{item.label}</span>
             <.command_shortcut>{item.shortcut}</.command_shortcut>
@@ -91,30 +93,24 @@ defmodule ZoonkDev.UIPreview.CommandPreviewLive do
       </.dialog>
 
       <.dialog id="groups-dialog">
-        <.command_input placeholder="Search commands..." />
+        <form phx-change="search-groups" phx-submit="search-groups">
+          <.command_input placeholder="Search commands..." />
+        </form>
 
         <.command_list>
-          <.command_group heading="Suggestions">
-            <.command_item>
-              <.icon name="tabler-calendar" />
-              <span>Calendar</span>
-            </.command_item>
-
-            <.command_item>
-              <.icon name="tabler-mood-happy" />
-              <span>Search Emoji</span>
-            </.command_item>
-
-            <.command_item>
-              <.icon name="tabler-calculator" />
-              <span>Calculator</span>
+          <.command_group :if={show_suggestions?(@suggestions_results)} heading="Suggestions">
+            <.command_item :for={item <- @suggestions_results}>
+              <.icon name={item.icon} />
+              <span>{item.label}</span>
             </.command_item>
           </.command_group>
 
-          <.command_separator />
+          <.command_separator :if={
+            show_suggestions?(@suggestions_results) and show_settings?(@groups_settings_results)
+          } />
 
-          <.command_group heading="Settings">
-            <.command_item :for={item <- settings()}>
+          <.command_group :if={show_settings?(@groups_settings_results)} heading="Settings">
+            <.command_item :for={item <- @groups_settings_results}>
               <.icon name={item.icon} />
               <span>{item.label}</span>
               <.command_shortcut>{item.shortcut}</.command_shortcut>
@@ -131,23 +127,67 @@ defmodule ZoonkDev.UIPreview.CommandPreviewLive do
     socket =
       socket
       |> assign(page_title: "Command")
-      |> assign(results: settings())
+      |> assign(doc_results: documentation_items())
+      |> assign(settings_results: settings())
+      |> assign(suggestions_results: suggestions())
+      |> assign(groups_settings_results: settings())
 
     {:ok, socket}
   end
 
-  def settings,
-    do: [
+  def documentation_items do
+    [
+      %{icon: "tabler-file-text", label: "Getting Started Guide"},
+      %{icon: "tabler-file-text", label: "Installation Tutorial"},
+      %{icon: "tabler-file-text", label: "API Reference"},
+      %{icon: "tabler-file-text", label: "Component Examples"},
+      %{icon: "tabler-file-text", label: "Best Practices"}
+    ]
+  end
+
+  def settings do
+    [
       %{icon: "tabler-user", label: "Account settings", shortcut: "⌘A"},
       %{icon: "tabler-bell", label: "Notifications", shortcut: "⌘N"},
       %{icon: "tabler-palette", label: "Appearance", shortcut: "⌘T"},
       %{icon: "tabler-shield", label: "Privacy & Security", shortcut: "⌘P"},
       %{icon: "tabler-language", label: "Language", shortcut: "⌘L"}
     ]
+  end
+
+  def suggestions do
+    [
+      %{icon: "tabler-calendar", label: "Calendar"},
+      %{icon: "tabler-mood-happy", label: "Search Emoji"},
+      %{icon: "tabler-calculator", label: "Calculator"}
+    ]
+  end
+
+  defp show_suggestions?([]), do: false
+  defp show_suggestions?(_items), do: true
+
+  defp show_settings?([]), do: false
+  defp show_settings?(_items), do: true
 
   @impl Phoenix.LiveView
-  def handle_event("search", %{"query" => query}, socket) do
+  def handle_event("search-docs", %{"query" => query}, socket) do
+    results = Helpers.fuzzy_search(documentation_items(), query, & &1.label)
+    {:noreply, assign(socket, doc_results: results)}
+  end
+
+  def handle_event("search-settings", %{"query" => query}, socket) do
     results = Helpers.fuzzy_search(settings(), query, & &1.label)
-    {:noreply, assign(socket, results: results)}
+    {:noreply, assign(socket, settings_results: results)}
+  end
+
+  def handle_event("search-groups", %{"query" => query}, socket) do
+    suggestions_results = Helpers.fuzzy_search(suggestions(), query, & &1.label)
+    groups_settings_results = Helpers.fuzzy_search(settings(), query, & &1.label)
+
+    {:noreply,
+     assign(socket,
+       suggestions_results: suggestions_results,
+       groups_settings_results: groups_settings_results
+     )}
   end
 end
