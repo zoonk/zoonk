e3248ea0f30d35a646957be784fb6f369d51c970
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-27 14:04:47 -0300

    Catalog: Refactor catalog home page tests

--- test/zoonk_web/live/catalog/catalog_home_live_test.exs
+++ test/zoonk_web/live/catalog/catalog_home_live_test.exs
@@ -5,8 +5,9 @@ defmodule ZoonkWeb.Catalog.CatalogHomeLiveTest do
 
   describe "catalog home page (unauthenticated)" do
     test "redirects to the login page", %{conn: conn} do
-      conn = get(conn, ~p"/catalog")
-      assert redirected_to(conn) == ~p"/login"
+      conn
+      |> visit(~p"/catalog")
+      |> assert_path(~p"/login")
     end
   end
 
@@ -14,15 +15,11 @@ defmodule ZoonkWeb.Catalog.CatalogHomeLiveTest do
     setup :signup_and_login_user
 
     test "renders page", %{conn: conn} do
-      {:ok, home_lv, _html} = live(conn, ~p"/")
-
-      {:ok, catalog_lv, _html} =
-        home_lv
-        |> element("aside a", "Catalog")
-        |> render_click()
-        |> follow_redirect(conn, ~p"/catalog")
-
-      assert has_element?(catalog_lv, "li[aria-current='page']", "Catalog")
+      conn
+      |> visit(~p"/catalog")
+      |> click_link("aside a", "Catalog")
+      |> assert_path(~p"/catalog")
+      |> assert_has("li[aria-current='page']", text: "Catalog")
     end
   end
 end
