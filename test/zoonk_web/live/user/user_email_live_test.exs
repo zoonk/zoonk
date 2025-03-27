feded7b51aa01e6e1f0ded2ccda7606d1d1f2051
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-27 15:01:14 -0300

    Tests: Refactor user email live tests

--- test/zoonk_web/live/user/user_email_live_test.exs
+++ test/zoonk_web/live/user/user_email_live_test.exs
@@ -1,92 +1,56 @@
 defmodule ZoonkWeb.User.UserEmailLiveTest do
   use ZoonkWeb.ConnCase, async: true
 
-  import Phoenix.LiveViewTest
   import Zoonk.AccountFixtures
 
   alias Zoonk.Accounts
 
+  describe "user email page (unauthenticated)" do
+    test "redirects to the login page", %{conn: conn} do
+      conn
+      |> visit(~p"/user/email")
+      |> assert_path(~p"/login")
+    end
+  end
+
   describe "user email page" do
-    test "renders settings page", %{conn: conn} do
-      {:ok, _lv, html} =
-        conn
-        |> login_user(user_fixture())
-        |> live(~p"/user/email")
+    setup :signup_and_login_user
 
-      assert html =~ "Change Email"
-    end
-
-    test "logout user", %{conn: conn} do
-      {:ok, lv, _html} =
-        conn
-        |> login_user(user_fixture())
-        |> live(~p"/user/email")
-
-      assert {:ok, _conn} =
-               lv
-               |> element("a", "Logout")
-               |> render_click()
-               |> follow_redirect(conn, ~p"/logout")
-    end
-
-    test "redirects if user is not logged in", %{conn: conn} do
-      assert {:error, redirect} = live(conn, ~p"/user/email")
-
-      assert {:redirect, %{to: path, flash: flash}} = redirect
-      assert path == ~p"/login"
-      assert %{"error" => "You must log in to access this page."} = flash
+    test "renders page", %{conn: conn} do
+      conn
+      |> visit(~p"/user/email")
+      |> assert_has("h3", text: "Change Email")
     end
   end
 
   describe "update email form" do
-    setup %{conn: conn} do
-      user = user_fixture()
-      %{conn: login_user(conn, user), user: user}
-    end
+    setup :signup_and_login_user
 
     test "updates the user email", %{conn: conn, user: user} do
       new_email = unique_user_email()
 
-      {:ok, lv, _html} = live(conn, ~p"/user/email")
+      conn
+      |> visit(~p"/user/email")
+      |> fill_in("Email address", with: new_email)
+      |> submit()
+      |> assert_has("div", text: "A link to confirm your email")
 
-      result =
-        lv
-        |> form("#email_form", %{
-          "user" => %{"email" => new_email}
-        })
-        |> render_submit()
-
-      assert result =~ "A link to confirm your email"
       assert Accounts.get_user_by_email(user.email)
     end
 
     test "renders errors with invalid data (phx-change)", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/user/email")
-
-      result =
-        lv
-        |> element("#email_form")
-        |> render_change(%{
-          "action" => "update_email",
-          "user" => %{"email" => "with spaces"}
-        })
-
-      assert result =~ "Change Email"
-      assert result =~ "must have the @ sign and no spaces"
+      conn
+      |> visit(~p"/user/email")
+      |> fill_in("Email address", with: "with spaces")
+      |> assert_has("p", text: "must have the @ sign and no spaces")
     end
 
     test "renders errors with invalid data (phx-submit)", %{conn: conn, user: user} do
-      {:ok, lv, _html} = live(conn, ~p"/user/email")
-
-      result =
-        lv
-        |> form("#email_form", %{
-          "user" => %{"email" => user.email}
-        })
-        |> render_submit()
-
-      assert result =~ "Change Email"
-      assert result =~ "did not change"
+      conn
+      |> visit(~p"/user/email")
+      |> fill_in("Email address", with: user.email)
+      |> submit()
+      |> assert_has("p", text: "did not change")
     end
   end
 
@@ -104,39 +68,35 @@ defmodule ZoonkWeb.User.UserEmailLiveTest do
     end
 
     test "updates the user email once", %{conn: conn, user: user, token: token, email: email} do
-      {:error, redirect} = live(conn, ~p"/user/email/confirm/#{token}")
+      conn
+      |> visit(~p"/user/email/confirm/#{token}")
+      |> assert_path(~p"/user/email")
+      |> assert_has("div", text: "Email changed successfully.")
 
-      assert {:live_redirect, %{to: path, flash: flash}} = redirect
-      assert path == ~p"/user/email"
-      assert %{"info" => message} = flash
-      assert message == "Email changed successfully."
       refute Accounts.get_user_by_email(user.email)
       assert Accounts.get_user_by_email(email)
 
       # use confirm token again
-      {:error, expired_redirect} = live(conn, ~p"/user/email/confirm/#{token}")
-      assert {:live_redirect, %{to: path, flash: flash}} = expired_redirect
-      assert path == ~p"/user/email"
-      assert %{"error" => message} = flash
-      assert message == "Email change link is invalid or it has expired."
+      conn
+      |> visit(~p"/user/email/confirm/#{token}")
+      |> assert_path(~p"/user/email")
+      |> assert_has("div", text: "Email change link is invalid or it has expired.")
     end
 
     test "does not update email with invalid token", %{conn: conn, user: user} do
-      {:error, redirect} = live(conn, ~p"/user/email/confirm/oops")
-      assert {:live_redirect, %{to: path, flash: flash}} = redirect
-      assert path == ~p"/user/email"
-      assert %{"error" => message} = flash
-      assert message == "Email change link is invalid or it has expired."
+      conn
+      |> visit(~p"/user/email/confirm/oops")
+      |> assert_path(~p"/user/email")
+      |> assert_has("div", text: "Email change link is invalid or it has expired.")
+
       assert Accounts.get_user_by_email(user.email)
     end
 
     test "redirects if user is not logged in", %{token: token} do
-      conn = build_conn()
-      {:error, redirect} = live(conn, ~p"/user/email/confirm/#{token}")
-      assert {:redirect, %{to: path, flash: flash}} = redirect
-      assert path == ~p"/login"
-      assert %{"error" => message} = flash
-      assert message == "You must log in to access this page."
+      build_conn()
+      |> visit(~p"/user/email/confirm/#{token}")
+      |> assert_path(~p"/login")
+      |> assert_has("div", text: "You must log in to access this page.")
     end
   end
 end
