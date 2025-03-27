96d34a3e7b1a7d0803104606f93bf5ce65fd2f12
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-27 15:09:54 -0300

    Tests: Refactor user signup live tests

--- test/zoonk_web/live/user/user_signup_live_test.exs
+++ test/zoonk_web/live/user/user_signup_live_test.exs
@@ -1,49 +1,38 @@
 defmodule ZoonkWeb.User.UserSignUpLiveTest do
   use ZoonkWeb.ConnCase, async: true
 
-  import Phoenix.LiveViewTest
   import Zoonk.AccountFixtures
 
   describe "Signup page" do
     test "renders signup page", %{conn: conn} do
-      {:ok, _lv, html} = live(conn, ~p"/signup")
-      assert html =~ "Sign up"
+      conn
+      |> visit(~p"/signup")
+      |> assert_has("h1", text: "Start learning")
     end
 
     test "redirects if already logged in", %{conn: conn} do
-      result =
-        conn
-        |> login_user(user_fixture())
-        |> live(~p"/signup")
-        |> follow_redirect(conn, ~p"/")
-
-      assert {:ok, _conn} = result
+      conn
+      |> login_user(user_fixture())
+      |> visit(~p"/signup")
+      |> assert_path(~p"/")
     end
   end
 
   describe "signup navigation" do
     test "redirects to login page when the Log in button is clicked", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/signup/email")
-
-      {:ok, login_live, _login_html} =
-        lv
-        |> element(~s|a:fl-contains("Login")|)
-        |> render_click()
-        |> follow_redirect(conn, ~p"/login")
-
-      assert has_element?(login_live, "a", "Login with Email")
+      conn
+      |> visit(~p"/signup/email")
+      |> click_link("Login")
+      |> assert_path(~p"/login")
+      |> assert_has("a", text: "Login with Email")
     end
 
     test "redirects to the sign up with email page", %{conn: conn} do
-      {:ok, lv, _html} = live(conn, ~p"/signup")
-
-      {:ok, signup_live, _signup_html} =
-        lv
-        |> element(~s|a:fl-contains("Sign up with Email")|)
-        |> render_click()
-        |> follow_redirect(conn, ~p"/signup/email")
-
-      assert has_element?(signup_live, "button", "Create an account")
+      conn
+      |> visit(~p"/signup")
+      |> click_link("Sign up with Email")
+      |> assert_path(~p"/signup/email")
+      |> assert_has("button", text: "Create an account")
     end
   end
 end
