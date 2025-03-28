463fa3f4c3b5e61fcf2b34276253f0978fc8380e
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-28 16:38:01 -0300

    Orgs: Add allowed_domains field to settings

--- lib/zoonk/orgs/org_settings.ex
+++ lib/zoonk/orgs/org_settings.ex
@@ -12,6 +12,7 @@ defmodule Zoonk.Orgs.OrgSettings do
 
   | Field Name | Type | Description |
   |------------|------|-------------|
+  | `allowed_domains` | `array` | Domains that allow users to automatically sign in/up. If empty, admins need to manually add users. |
   | `currency` | `Ecto.Enum` | The currency used by the organization. |
   | `stripe_customer_id` | `String` | The Stripe customer ID for the organization. |
   | `tax_id` | `Zoonk.Vault.Binary` | The tax ID for the organization, stored securely. |
@@ -28,6 +29,7 @@ defmodule Zoonk.Orgs.OrgSettings do
   alias Zoonk.Vault
 
   schema "org_settings" do
+    field :allowed_domains, {:array, :string}, default: []
     field :currency, Ecto.Enum, values: CurrencyConfig.list_currencies(:atom), default: :USD
     field :stripe_customer_id, :string
     field :tax_id, Vault.Binary
@@ -40,7 +42,7 @@ defmodule Zoonk.Orgs.OrgSettings do
   @doc false
   def changeset(org, attrs) do
     org
-    |> cast(attrs, [:currency, :org_id, :stripe_customer_id, :tax_id])
+    |> cast(attrs, [:currency, :org_id, :stripe_customer_id, :tax_id, :allowed_domains])
     |> validate_required([:currency, :org_id])
     |> unique_constraint(:org_id)
   end
