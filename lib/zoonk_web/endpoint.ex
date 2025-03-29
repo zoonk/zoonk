6b993ddf9fb677838e9fb4e40be6b35988007c82
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-29 18:01:15 -0300

    LiveView: Fix socket connection info

--- lib/zoonk_web/endpoint.ex
+++ lib/zoonk_web/endpoint.ex
@@ -12,8 +12,8 @@ defmodule ZoonkWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]],
-    longpoll: [connect_info: [session: @session_options]]
+    websocket: [connect_info: [:uri, session: @session_options]],
+    longpoll: [connect_info: [:uri, session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
   #
