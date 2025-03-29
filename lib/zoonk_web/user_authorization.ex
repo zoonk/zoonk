c07e8e6386b89377c604996397580f94268f5d5d
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-29 15:12:19 -0300

    UserAuthorization: Add organization member hook

--- lib/zoonk_web/user_authorization.ex
+++ lib/zoonk_web/user_authorization.ex
@@ -5,6 +5,7 @@ defmodule ZoonkWeb.UserAuthorization do
   This module contains plugs that check if a user has permission to access
   certain routes based on their organization membership and status.
   """
+  use ZoonkWeb, :verified_routes
   use Gettext, backend: Zoonk.Gettext
 
   import Phoenix.Controller
@@ -37,6 +38,37 @@ defmodule ZoonkWeb.UserAuthorization do
     |> put_flash(:error, dgettext("errors", "You must be a member of this organization."))
     |> UserAuth.logout_user()
     |> halt()
+  end
+
+  @doc """
+  LiveView hook to check if the user is a confirmed member of the current organization.
+
+  This hook verifies that either:
+  1. The user is a confirmed member of the organization
+  2. OR the organization is public (`:app` or `:creator` kind)
+
+  If not, then we log them out and redirect to the home page.
+
+  ## Examples
+
+      on_mount {ZoonkWeb.UserAuthorization, :ensure_org_member}
+  """
+  def on_mount(:ensure_org_member, _params, _session, socket)
+      when socket.assigns.current_scope.org.kind in [:app, :creator] do
+    {:cont, socket}
+  end
+
+  def on_mount(:ensure_org_member, _params, _session, socket) do
+    if org_member?(socket.assigns.current_scope) do
+      {:cont, socket}
+    else
+      socket =
+        socket
+        |> Phoenix.LiveView.put_flash(:error, dgettext("errors", "You must be a member of this organization."))
+        |> Phoenix.LiveView.redirect(to: ~p"/login")
+
+      {:halt, socket}
+    end
   end
 
   defp org_member?(%Scope{user: %User{confirmed_at: nil}}), do: false
