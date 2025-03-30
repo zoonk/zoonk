31bd87bfc53ec7cbf40468b883be29cbf966ed23
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-30 16:36:34 -0300

    Tests: Simplify catalog and library home tests

--- test/zoonk_web/live/catalog/catalog_home_live_test.exs
+++ test/zoonk_web/live/catalog/catalog_home_live_test.exs
@@ -1,23 +1,11 @@
 defmodule ZoonkWeb.Catalog.CatalogHomeLiveTest do
   use ZoonkWeb.ConnCase, async: true
 
-  describe "catalog home page (unauthenticated)" do
-    test "redirects to the login page", %{conn: conn} do
-      conn
-      |> visit(~p"/catalog")
-      |> assert_path(~p"/login")
-    end
-  end
+  import ZoonkWeb.OrgMemberRequiredHelper
 
   describe "catalog home page" do
-    setup :signup_and_login_user
-
-    test "renders page", %{conn: conn} do
-      conn
-      |> visit(~p"/catalog")
-      |> click_link("aside a", "Catalog")
-      |> assert_path(~p"/catalog")
-      |> assert_has("li[aria-current='page']", text: "Catalog")
+    test "handle org authorization" do
+      assert_page_authorization(%{link: ~p"/catalog", title: "Catalog"})
     end
   end
 end
--- test/zoonk_web/live/library/library_home_live_test.exs
+++ test/zoonk_web/live/library/library_home_live_test.exs
@@ -1,23 +1,11 @@
 defmodule ZoonkWeb.Library.LibraryHomeLiveTest do
   use ZoonkWeb.ConnCase, async: true
 
-  describe "library home page (unauthenticated)" do
-    test "redirects to the login page", %{conn: conn} do
-      conn
-      |> visit(~p"/library")
-      |> assert_path(~p"/login")
-    end
-  end
+  import ZoonkWeb.OrgMemberRequiredHelper
 
   describe "library home page" do
-    setup :signup_and_login_user
-
-    test "renders page", %{conn: conn} do
-      conn
-      |> visit(~p"/")
-      |> click_link("aside a", "Library")
-      |> assert_path(~p"/library")
-      |> assert_has("li[aria-current='page']", text: "Library")
+    test "handle org authorization" do
+      assert_page_authorization(%{link: ~p"/library", title: "Library"})
     end
   end
 end
