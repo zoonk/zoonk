54bc660868bddfa2816b65661bd6d41f3670a9c8
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-30 19:01:37 -0300

    Accounts: Add org member creation on signup

--- lib/zoonk/accounts.ex
+++ lib/zoonk/accounts.ex
@@ -18,6 +18,7 @@ defmodule Zoonk.Accounts do
   alias Zoonk.Config.AuthConfig
   alias Zoonk.Helpers
   alias Zoonk.Orgs.Org
+  alias Zoonk.Orgs.OrgMember
   alias Zoonk.Orgs.OrgSettings
   alias Zoonk.Repo
   alias Zoonk.Scope
@@ -72,6 +73,13 @@ defmodule Zoonk.Accounts do
     Ecto.Multi.new()
     |> Ecto.Multi.insert(:user, changeset)
     |> Ecto.Multi.insert(:profile, &build_initial_user_profile/1)
+    |> Ecto.Multi.insert(:org_member, fn %{user: user} ->
+      OrgMember.changeset(%OrgMember{}, %{
+        org_id: scope.org.id,
+        user_id: user.id,
+        role: :member
+      })
+    end)
     |> Repo.transaction()
     |> Helpers.get_changeset_from_transaction(:user)
   end
@@ -308,6 +316,13 @@ defmodule Zoonk.Accounts do
     |> Ecto.Multi.insert(:user, user_changeset)
     |> Ecto.Multi.insert(:profile, &build_initial_user_profile(&1, profile_opts))
     |> Ecto.Multi.insert(:provider, &user_provider_changeset(&1, provider_attrs))
+    |> Ecto.Multi.insert(:org_member, fn %{user: user} ->
+      OrgMember.changeset(%OrgMember{}, %{
+        org_id: scope.org.id,
+        user_id: user.id,
+        role: :member
+      })
+    end)
     |> Repo.transaction()
     |> Helpers.get_changeset_from_transaction(:user)
   end
