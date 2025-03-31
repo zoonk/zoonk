a69c1fbccd0ce62de2aa740e42ad815b275f4de2
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-03-31 17:45:02 -0300

    add more tests for menu items

--- test/zoonk_web/permissions/menu_visibility_test.exs
+++ test/zoonk_web/permissions/menu_visibility_test.exs
@@ -1,6 +1,7 @@
 defmodule ZoonkWeb.MenuVisibilityTest do
   use ZoonkWeb.ConnCase, async: true
 
+  import Zoonk.AccountFixtures
   import Zoonk.OrgFixtures
 
   describe "Menu visibility" do
@@ -19,6 +20,63 @@ defmodule ZoonkWeb.MenuVisibilityTest do
       |> refute_has("a", text: "Organization")
       |> refute_has("a", text: "Settings")
       |> refute_has("a", text: "Logout")
+    end
+
+    test "guest user menu items", %{conn: conn} do
+      org = org_fixture(%{kind: :app})
+      user = user_fixture(%{kind: :guest})
+      org_member_fixture(%{org: org, user: user, role: :member})
+
+      conn
+      |> login_user(user)
+      |> assert_has("a", text: "Catalog")
+      |> refute_has("a", text: "Login")
+      |> refute_has("a", text: "Sign Up")
+      |> assert_has("a", text: "Summary")
+      |> assert_has("a", text: "Goals")
+      |> assert_has("a", text: "Library")
+      |> refute_has("a", text: "Editor")
+      |> refute_has("a", text: "Organization")
+      |> refute_has("a", text: "Settings")
+      |> refute_has("a", text: "Logout")
+    end
+
+    test "regular user menu items", %{conn: conn} do
+      org = org_fixture(%{kind: :app})
+      user = user_fixture(%{kind: :regular})
+      org_member_fixture(%{org: org, user: user, role: :member})
+
+      conn
+      |> login_user(user)
+      |> assert_has("a", text: "Catalog")
+      |> refute_has("a", text: "Login")
+      |> refute_has("a", text: "Sign Up")
+      |> assert_has("a", text: "Summary")
+      |> assert_has("a", text: "Goals")
+      |> assert_has("a", text: "Library")
+      |> refute_has("a", text: "Editor")
+      |> refute_has("a", text: "Organization")
+      |> assert_has("a", text: "Settings")
+      |> assert_has("a", text: "Logout")
+    end
+
+    test "admin user menu items", %{conn: conn} do
+      org = org_fixture(%{kind: :app})
+      user = user_fixture()
+      org_member_fixture(%{org: org, user: user, role: :admin})
+
+      conn
+      |> login_user(user)
+      |> assert_has("a", text: "Catalog")
+      |> refute_has("a", text: "Login")
+      |> refute_has("a", text: "Sign Up")
+      |> assert_has("a", text: "Summary")
+      |> assert_has("a", text: "Goals")
+      |> assert_has("a", text: "Library")
+      |> assert_has("a", text: "Editor")
+      |> assert_has("a", text: "Organization")
+      |> assert_has("a", text: "Settings")
+      |> assert_has("a", text: "Logout")
     end
   end
 end
