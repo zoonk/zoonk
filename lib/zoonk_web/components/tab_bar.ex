59d7abc794601a36d628c01dabf8525b1fd9ee3b
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-01 21:28:01 -0300

    move tab bar to top on tablet

--- lib/zoonk_web/components/tab_bar.ex
+++ lib/zoonk_web/components/tab_bar.ex
@@ -15,10 +15,13 @@ defmodule ZoonkWeb.Components.TabBar do
   def tab_bar(assigns) do
     ~H"""
     <nav class={[
-      "fixed right-0 bottom-0 left-0 lg:hidden",
+      "fixed right-0 bottom-0 left-0 w-full lg:hidden",
       "bg-zk-background/80 backdrop-blur-lg",
       "border-zk-border border-t",
       "z-50",
+      "md:sticky md:top-4 md:bottom-auto",
+      "md:bg-zk-secondary md:rounded-full md:border-0",
+      "md:min-w-md md:mx-auto md:max-w-xl md:py-1.5",
       @class
     ]}>
       <ul class="flex items-center justify-around px-2">
@@ -38,15 +41,16 @@ defmodule ZoonkWeb.Components.TabBar do
     <li class="flex-1">
       <.link
         class={[
-          "flex flex-col items-center gap-1 px-3 py-2",
-          "text-sm transition-colors",
-          @active && "text-zk-primary-text",
-          !@active && "text-zk-muted-foreground"
+          "flex flex-col items-center gap-1 px-3 py-1.5 transition-colors",
+          "text-xs font-medium md:text-sm",
+          "md:rounded-full md:hover:bg-zk-background",
+          @active && "text-zk-primary bg-zk-background md:font-semibold",
+          !@active && "text-zk-muted-foreground md:text-zk-secondary-foreground md:font-normal"
         ]}
         {@rest}
       >
-        <.icon name={@icon} size={:md} />
-        <span class="text-xs font-medium">{@label}</span>
+        <.icon name={@icon} size={:md} class="md:hidden" />
+        <span>{@label}</span>
       </.link>
     </li>
     """
