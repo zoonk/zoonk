60ccf7de10387f19ac53471f0243c6f81666c185
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-09 18:00:58 -0300

    Onboarding: Update onboarding start page layout

--- lib/zoonk_web/live/onboarding/onboarding_start_live.ex
+++ lib/zoonk_web/live/onboarding/onboarding_start_live.ex
@@ -7,10 +7,23 @@ defmodule ZoonkWeb.Onboarding.OnboardingStartLive do
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <div>
-      placeholder for onboarding
-      <h1>{dgettext("onboarding", "Get Started")}</h1>
-    </div>
+    <main class="h-dvh flex w-full items-center justify-center p-4">
+      <div class="flex w-full max-w-lg flex-col items-center gap-4 text-center">
+        <.text tag="h1" size={:xxl}>
+          {dgettext("onboarding", "What do you want to learn?")}
+        </.text>
+
+        <form class="w-full">
+          <.input
+            name="subject"
+            type="text"
+            value=""
+            class="w-full"
+            placeholder={dgettext("onboarding", "E.g. Computer Science, Astronomy, Biology, etc.")}
+          />
+        </form>
+      </div>
+    </main>
     """
   end
 
