985e49d17a290d4cf08a57d58ea6a2c420170474
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-14 14:58:59 -0300

    Config: Remove Gettext from category configuration

--- lib/zoonk/config/category_config.ex
+++ lib/zoonk/config/category_config.ex
@@ -5,26 +5,25 @@ defmodule Zoonk.Config.CategoryConfig do
   This module centralizes all course category settings used throughout
   the application, ensuring consistency and ease of maintenance.
   """
-  use Gettext, backend: Zoonk.Gettext
 
   # styler:sort
   @supported_categories [
-    astronomy: gettext("Astronomy"),
-    biology: gettext("Biology"),
-    business: gettext("Business"),
-    chemistry: gettext("Chemistry"),
-    communication: gettext("Communication"),
-    culture: gettext("Culture"),
-    design: gettext("Design"),
-    economics: gettext("Economics"),
-    engineering: gettext("Engineering"),
-    geography: gettext("Geography"),
-    health: gettext("Health"),
-    history: gettext("History"),
-    math: gettext("Math"),
-    physics: gettext("Physics"),
-    society: gettext("Society"),
-    tech: gettext("Tech")
+    :astronomy,
+    :biology,
+    :business,
+    :chemistry,
+    :communication,
+    :culture,
+    :design,
+    :economics,
+    :engineering,
+    :geography,
+    :health,
+    :history,
+    :math,
+    :physics,
+    :society,
+    :tech
   ]
 
   @doc """
@@ -40,15 +39,11 @@ defmodule Zoonk.Config.CategoryConfig do
       iex> list_categories(:options)
       [{"Physics", "physics"}, {"Communication", "communication"}, {"History", "history"}, ...]
   """
-  def list_categories(:atom) do
-    Enum.map(@supported_categories, fn {key, _value} -> key end)
-  end
+  def list_categories(:atom), do: @supported_categories
 
-  def list_categories(:string) do
-    Enum.map(@supported_categories, fn {key, _value} -> Atom.to_string(key) end)
-  end
+  def list_categories(:string), do: Enum.map(@supported_categories, &Atom.to_string/1)
 
   def list_categories(:options) do
-    Enum.map(@supported_categories, fn {key, value} -> {value, Atom.to_string(key)} end)
+    Enum.map(@supported_categories, fn key -> {key, Atom.to_string(key)} end)
   end
 end
