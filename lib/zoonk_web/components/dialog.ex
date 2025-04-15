a3ac1557c194731cd842ad2fb3f86b767df171f8
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-15 18:29:46 -0300

    Dialog: Update dialog border styling

--- lib/zoonk_web/components/dialog.ex
+++ lib/zoonk_web/components/dialog.ex
@@ -26,7 +26,7 @@ defmodule ZoonkWeb.Components.Dialog do
       id={@id}
       class={[
         "max-h-[85vh] w-[90vw] overflow-hidden",
-        "fixed z-50 rounded p-0 shadow-lg",
+        "ring-zk-border/60 fixed z-50 rounded p-0 shadow-lg ring",
         "bg-zk-surface text-zk-surface-foreground",
         "backdrop:bg-zk-background/20 backdrop:backdrop-blur-sm",
         "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
