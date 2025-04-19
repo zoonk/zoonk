deecdcaf072776527af0bb0653542657969cf3b7
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-19 15:53:47 -0300

    Tests: Add nested object support test

--- test/zoonk/ai/ai_schema_test.exs
+++ test/zoonk/ai/ai_schema_test.exs
@@ -22,5 +22,24 @@ defmodule Zoonk.AI.AISchemaTest do
       assert ai_schema.schema.properties.age.type == "integer"
       assert ai_schema.schema.properties.public.type == "boolean"
     end
+
+    test "supports nested objects" do
+      ai_schema = AISchema.add_field(%AISchema{}, %{address: %{street: "string", city: "string"}})
+
+      assert ai_schema.type == "json_schema"
+      assert ai_schema.name == ""
+      assert ai_schema.strict == true
+
+      assert ai_schema.schema.type == "object"
+      assert ai_schema.schema.required == ["address"]
+      assert ai_schema.schema.additionalProperties == false
+
+      assert ai_schema.schema.properties.address.type == "object"
+      assert ai_schema.schema.properties.address.required == ["street", "city"]
+      assert ai_schema.schema.properties.address.additionalProperties == false
+
+      assert ai_schema.schema.properties.address.properties.street.type == "string"
+      assert ai_schema.schema.properties.address.properties.city.type == "string"
+    end
   end
 end
