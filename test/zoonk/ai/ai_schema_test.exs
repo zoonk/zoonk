0458d599a4a18544d62cfb1b4ac7ce044f80bc2b
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-19 16:32:36 -0300

    Tests: Add support for multiple array fields

--- test/zoonk/ai/ai_schema_test.exs
+++ test/zoonk/ai/ai_schema_test.exs
@@ -79,5 +79,34 @@ defmodule Zoonk.AI.AISchemaTest do
       assert ai_schema.schema.properties.courses.items.properties.title.type == "string"
       assert ai_schema.schema.properties.courses.items.properties.description.type == "string"
     end
+
+    test "supports multiple array fields" do
+      ai_schema =
+        %AISchema{}
+        |> AISchema.add_field(%{courses: [%{title: "string", description: "string"}]})
+        |> AISchema.add_field(%{projects: [%{name: "string", status: "string"}]})
+
+      assert ai_schema.type == "json_schema"
+      assert ai_schema.name == ""
+      assert ai_schema.strict == true
+
+      assert ai_schema.schema.type == "object"
+      assert ai_schema.schema.required == ["courses", "projects"]
+      assert ai_schema.schema.additionalProperties == false
+
+      assert ai_schema.schema.properties.courses.type == "array"
+      assert ai_schema.schema.properties.courses.items.type == "object"
+      assert ai_schema.schema.properties.courses.items.required == ["description", "title"]
+      assert ai_schema.schema.properties.courses.items.additionalProperties == false
+      assert ai_schema.schema.properties.courses.items.properties.title.type == "string"
+      assert ai_schema.schema.properties.courses.items.properties.description.type == "string"
+
+      assert ai_schema.schema.properties.projects.type == "array"
+      assert ai_schema.schema.properties.projects.items.type == "object"
+      assert ai_schema.schema.properties.projects.items.required == ["name", "status"]
+      assert ai_schema.schema.properties.projects.items.additionalProperties == false
+      assert ai_schema.schema.properties.projects.items.properties.name.type == "string"
+      assert ai_schema.schema.properties.projects.items.properties.status.type == "string"
+    end
   end
 end
