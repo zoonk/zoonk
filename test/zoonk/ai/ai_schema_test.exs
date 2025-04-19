d96462c69fa53fc11f024a2644eb63e38f56e1e6
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-19 15:04:05 -0300

    Tests: Update AISchemaTest assertions

--- test/zoonk/ai/ai_schema_test.exs
+++ test/zoonk/ai/ai_schema_test.exs
@@ -8,6 +8,10 @@ defmodule Zoonk.AI.AISchemaTest do
       schema = %AISchema{}
       updated_schema = AISchema.add_field(schema, %{name: "string", age: "integer"})
 
+      assert updated_schema.type == "json_schema"
+      assert updated_schema.name == ""
+      assert updated_schema.strict == true
+      assert updated_schema.schema.type == "object"
       assert updated_schema.schema.properties.name.type == "string"
       assert updated_schema.schema.properties.age.type == "integer"
       assert updated_schema.schema.required == ["name", "age"]
