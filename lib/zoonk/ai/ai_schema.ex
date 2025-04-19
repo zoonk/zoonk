acbbe49fa1c5b10caee94d2edd114bced1cacbaf
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-19 16:24:31 -0300

    AISchema: Update courses field example

--- lib/zoonk/ai/ai_schema.ex
+++ lib/zoonk/ai/ai_schema.ex
@@ -16,9 +16,7 @@ defmodule Zoonk.AI.AISchema do
 
       # Creating a list
       address = %AISchema{}
-      |> AISchema.add_field(%{courses: "array"})
-      |> AISchema.add_field(courses: %{title: "string"})
-      |> AISchema.add_field(courses: %{description: "string"})
+      |> AISchema.add_field(%{courses: [%{title: "string", description: "string"}]})
 
       # Passing options
       %AISchema{}
@@ -39,6 +37,9 @@ defmodule Zoonk.AI.AISchema do
 
       iex> AISchema.add_field(%AISchema{}, %{age: "integer", name: "string"})
       %AISchema{schema: %{properties: %{age: %{type: "integer"}, name: %{type: "string"}}}}
+
+      iex> AISchema.add_field(%AISchema{}, %{courses: [%{title: "string"}]})
+      %AISchema{schema: %{properties: %{courses: %{type: "array", items: %{type: "object", properties: %{title: %{type: "string"}}}}}}}
   """
   def add_field(%__MODULE__{schema: schema} = ai_schema, %{} = fields) do
     %{ai_schema | schema: merge_fields(schema, fields)}
@@ -62,6 +63,21 @@ defmodule Zoonk.AI.AISchema do
        properties: nested_props,
        required: keys_to_string(nested_props),
        additionalProperties: false
+     }}
+  end
+
+  defp fields_to_json_props({key, [%{} = item_schema]}) do
+    items_props = fields_to_json_props(item_schema)
+
+    {key,
+     %{
+       type: "array",
+       items: %{
+         type: "object",
+         properties: items_props,
+         required: keys_to_string(items_props),
+         additionalProperties: false
+       }
      }}
   end
 
