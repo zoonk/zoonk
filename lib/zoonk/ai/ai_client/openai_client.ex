7df4e6c9f1e3096be9a7978a11917c5158825b11
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-19 19:53:27 -0300

    AI: Update OpenAIClient with error handling

--- lib/zoonk/ai/ai_client/openai_client.ex
+++ lib/zoonk/ai/ai_client/openai_client.ex
@@ -22,10 +22,27 @@ defmodule Zoonk.AI.AIClient.OpenAIClient do
       {:ok, %Req.Response{}}
   """
   def generate_object(%AI{} = payload) do
-    Req.post(@responses_endpoint, json: payload, auth: {:bearer, get_api_key()})
+    case Req.post(@responses_endpoint, json: payload, auth: {:bearer, get_api_key()}) do
+      {:ok, %Req.Response{body: %{"error" => nil} = body}} ->
+        object_response(hd(body["output"]["content"]))
+
+      {:ok, %Req.Response{body: %{"error" => error}}} ->
+        {:error, error}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  defp object_response([%{"type" => "output_text"} = content]) do
+    {:ok, JSON.decode!(content["text"])}
+  end
+
+  defp object_response([%{"type" => "refusal"} = content]) do
+    {:error, content["refusal"]}
   end
 
   defp get_api_key do
-    Application.get_env(:zoonk, :ai)[:openai][:api_key]
+    Application.get_env(:zoonk, :openai)[:api_key]
   end
 end
---
+++ test/zoonk/ai/ai_client/openai_client_test.exs
@@ -0,0 +1,25 @@
+defmodule Zoonk.AI.AIClient.OpenAIClientTest do
+  use Zoonk.DataCase, async: true
+
+  alias Zoonk.AI.AIClient.OpenAIClient
+
+  describe "generate_object/1" do
+    test "returns the object when successful" do
+      Req.Test.stub(Zoonk.AI.AIClient.OpenAIClient, fn conn ->
+        Req.Test.json(conn, %{
+          "error" => nil,
+          "output" => [
+            %{
+              "content" => [
+                %{"type" => "output_text", "text" => ~s({"language":"English"})}
+              ]
+            }
+          ]
+        })
+      end)
+
+      assert {:ok, %{"language" => "English"}} =
+               OpenAIClient.generate_object(%Zoonk.AI{})
+    end
+  end
+end
