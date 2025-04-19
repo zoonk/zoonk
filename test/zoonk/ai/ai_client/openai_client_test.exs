d4d76baecb730ba50fd85fc813c6dc79111d92b9
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-19 20:50:18 -0300

    Tests: Refactor mock response in OpenAIClientTest

--- test/zoonk/ai/ai_client/openai_client_test.exs
+++ test/zoonk/ai/ai_client/openai_client_test.exs
@@ -5,20 +5,23 @@ defmodule Zoonk.AI.AIClient.OpenAIClientTest do
 
   describe "generate_object/1" do
     test "returns the object when successful" do
-      Req.Test.stub(:openai_client, fn conn ->
-        Req.Test.json(conn, %{
-          "error" => nil,
-          "output" => [
-            %{
-              "content" => [
-                %{"type" => "output_text", "text" => ~s({"language":"English"})}
-              ]
-            }
-          ]
-        })
-      end)
-
+      mock_response(~s({"language":"English"}))
       assert {:ok, %{language: "English"}} = OpenAIClient.generate_object(%Zoonk.AI{})
     end
+  end
+
+  defp mock_response(mock_data) do
+    Req.Test.stub(:openai_client, fn conn ->
+      Req.Test.json(conn, %{
+        "error" => nil,
+        "output" => [
+          %{
+            "content" => [
+              %{"type" => "output_text", "text" => mock_data}
+            ]
+          }
+        ]
+      })
+    end)
   end
 end
