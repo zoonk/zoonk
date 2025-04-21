6cfc13bc8d882a8b4537177b30577011a2bcd951
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-21 18:53:28 -0300

    Add loading draft for recommendations

--- lib/zoonk_web/live/onboarding/onboarding_recommendations_live.ex
+++ lib/zoonk_web/live/onboarding/onboarding_recommendations_live.ex
@@ -7,7 +7,30 @@ defmodule ZoonkWeb.Onboarding.OnboardingRecommendationsLive do
   @impl Phoenix.LiveView
   def render(assigns) do
     ~H"""
-    <main class="h-dvh flex"></main>
+    <main class="h-dvh flex flex-col items-center justify-center">
+      <div
+        :if={@recommendations == []}
+        class="flex w-full max-w-md flex-col items-center justify-center p-8 text-center"
+      >
+        <div class="mb-8">
+          <div class="border-zk-primary mx-auto h-16 w-16 animate-spin rounded-full border-t-4"></div>
+        </div>
+
+        <.text tag="h2" size={:xl} class="mb-4">
+          {dgettext("onboarding", "We're finding specializations that will help you learn")}
+          <em class="text-zk-primary">{@input}</em>
+        </.text>
+
+        <.text variant={:secondary}>
+          {dgettext(
+            "onboarding",
+            "This might take a moment as we prepare personalized recommendations for you."
+          )}
+        </.text>
+      </div>
+
+      <div :if={@recommendations != []} class="w-full"></div>
+    </main>
     """
   end
 
