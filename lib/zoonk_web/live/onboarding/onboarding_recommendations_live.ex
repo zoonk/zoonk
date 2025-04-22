2503b18367862cf8019a0887c85d1f7cef91c74b
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-22 15:10:41 -0300

    Onboarding: Add aria-hidden attribute to icon container

--- lib/zoonk_web/live/onboarding/onboarding_recommendations_live.ex
+++ lib/zoonk_web/live/onboarding/onboarding_recommendations_live.ex
@@ -76,7 +76,10 @@ defmodule ZoonkWeb.Onboarding.OnboardingRecommendationsLive do
               </.text>
             </div>
 
-            <div class="size-17 bg-zk-secondary/75 flex shrink-0 flex-col items-center justify-center rounded-lg">
+            <div
+              aria-hidden
+              class="size-17 bg-zk-secondary/75 flex shrink-0 flex-col items-center justify-center rounded-lg"
+            >
               <.dynamic_icon
                 name={recommendation.icon || "tabler-book"}
                 variant={:filled}
