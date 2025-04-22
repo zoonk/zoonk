a09eca38cc04a8c5d17ea5292ddba7b361c31ecb
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-21 21:41:48 -0300

    Use async for loading recommendations

--- lib/zoonk_web/live/onboarding/onboarding_recommendations_live.ex
+++ lib/zoonk_web/live/onboarding/onboarding_recommendations_live.ex
@@ -2,6 +2,8 @@ defmodule ZoonkWeb.Onboarding.OnboardingRecommendationsLive do
   @moduledoc false
   use ZoonkWeb, :live_view
 
+  alias Zoonk.AI.Agents.OnboardingRecommender
+
   on_mount {ZoonkWeb.Onboarding.OnboardingPermissions, :onboarding_permissions}
 
   @impl Phoenix.LiveView
@@ -9,7 +11,7 @@ defmodule ZoonkWeb.Onboarding.OnboardingRecommendationsLive do
     ~H"""
     <main class="h-dvh flex flex-col items-center justify-center">
       <.full_page_spinner
-        :if={@recommendations == []}
+        :if={@courses.loading}
         title={dgettext("onboarding", "We're finding specializations that will help you learn")}
         feature={@input}
         subtitle={
@@ -20,20 +22,25 @@ defmodule ZoonkWeb.Onboarding.OnboardingRecommendationsLive do
         }
       />
 
-      <div :if={@recommendations != []} class="w-full"></div>
+      <div :if={@courses.ok?} class="w-full">
+        <div :for={recommendation <- @courses.result} class="course-item">
+          <p>{recommendation.title}</p>
+        </div>
+      </div>
     </main>
     """
   end
 
   @impl Phoenix.LiveView
-  def mount(params, _session, socket) do
+  def mount(params, session, socket) do
     input = params["input"]
+    language = session["language"]
 
     socket =
       socket
       |> assign(:page_title, dgettext("onboarding", "Recommendations for %{input}", input: input))
       |> assign(:input, input)
-      |> assign(:recommendations, [])
+      |> assign_async(:courses, fn -> OnboardingRecommender.recommend(input, language) end)
 
     {:ok, socket}
   end
