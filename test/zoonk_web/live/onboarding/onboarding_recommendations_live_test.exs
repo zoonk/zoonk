3f74d042eaa7fd59161ff09ff1b156771743cc2c
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-22 15:30:04 -0300

    Tests: Add onboarding start page tests for guests

--- test/zoonk_web/live/onboarding/onboarding_recommendations_live_test.exs
+++ test/zoonk_web/live/onboarding/onboarding_recommendations_live_test.exs
@@ -1,7 +1,12 @@
 defmodule ZoonkWeb.OnboardingRecommendationsLiveTest do
   use ZoonkWeb.ConnCase, async: true
 
+  import Zoonk.CatalogFixtures
   import Zoonk.OrgFixtures
+
+  alias Zoonk.Accounts
+  alias Zoonk.Catalog
+  alias Zoonk.Scope
 
   describe "onboarding recommendations (unauthenticated)" do
     test "redirects to /start for :app org" do
@@ -30,6 +35,41 @@ defmodule ZoonkWeb.OnboardingRecommendationsLiveTest do
       |> Map.put(:host, org_fixture(%{kind: :school}).custom_domain)
       |> visit(~p"/start")
       |> assert_path(~p"/login")
+    end
+  end
+
+  describe "onboarding start page (guest user)" do
+    setup do
+      app_org = app_org_fixture()
+      conn = Map.put(build_conn(), :host, app_org.custom_domain)
+      %{conn: conn, org: app_org}
+    end
+
+    test "allows guest user without courses to see the page", %{conn: conn, org: org} do
+      {:ok, user} = Accounts.create_guest_user(%{language: "en"}, %Scope{org: org, user: nil})
+
+      # Verify the user is not enrolled in a course
+      refute Catalog.user_enrolled_in_any_course?(user.id)
+
+      conn
+      |> login_user(user)
+      |> visit(~p"/start/coding")
+      |> assert_path(~p"/start/coding")
+      |> assert_has("h2", text: "We're finding specializations")
+    end
+
+    test "redirects guest user with courses to the home page", %{conn: conn, org: org} do
+      {:ok, user} = Accounts.create_guest_user(%{language: "en"}, %Scope{org: org, user: nil})
+      course = course_fixture()
+      course_user_fixture(%{user_id: user.id, course_id: course.id})
+
+      # Verify the user is enrolled in a course
+      assert Catalog.user_enrolled_in_any_course?(user.id)
+
+      conn
+      |> login_user(user)
+      |> visit(~p"/start/coding")
+      |> assert_path(~p"/")
     end
   end
 end
