cc9adc2621756d67425ad89ffe3eef896a225d78
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-28 13:10:01 -0300

    Catalog: Update navigation user visibility

--- lib/zoonk_web/live/catalog/catalog_home_live.ex
+++ lib/zoonk_web/live/catalog/catalog_home_live.ex
@@ -6,8 +6,12 @@ defmodule ZoonkWeb.Catalog.CatalogHomeLive do
   def render(assigns) do
     ~H"""
     <main>
-      <nav class="flex items-center justify-between gap-4 p-4">
-        <.back_link navigate={~p"/"} />
+      <nav class={[
+        "flex items-center gap-4 p-4",
+        @scope.user && "justify-between",
+        !@scope.user && "justify-end"
+      ]}>
+        <.back_link :if={@scope.user} navigate={~p"/"} />
 
         <.a :if={!@scope.user} kind={:button} variant={:outline} href={~p"/login"}>
           {gettext("Login")}
