92c257359819c2f54688e767fcd958a24e3b48f8
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-04-30 14:47:58 -0300

    add label to catalog nav

--- lib/zoonk_web/live/catalog/catalog_home_live.ex
+++ lib/zoonk_web/live/catalog/catalog_home_live.ex
@@ -6,11 +6,14 @@ defmodule ZoonkWeb.Catalog.CatalogHomeLive do
   def render(assigns) do
     ~H"""
     <main>
-      <nav class={[
-        "flex items-center gap-4 p-4",
-        @scope.user && "justify-between",
-        !@scope.user && "justify-end"
-      ]}>
+      <nav
+        class={[
+          "flex items-center gap-4 p-4",
+          @scope.user && "justify-between",
+          !@scope.user && "justify-end"
+        ]}
+        aria-label={gettext("Actions")}
+      >
         <.back_link :if={@scope.user} navigate={~p"/"} />
 
         <.a :if={!@scope.user} kind={:button} variant={:outline} href={~p"/login"}>
