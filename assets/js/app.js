25076a7cae28b7a40d9da9362132ec0b05ae64e3
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-05-01 11:45:32 -0300

    only use posthog in production

--- assets/js/app.js
+++ assets/js/app.js
@@ -65,21 +65,23 @@ topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
 window.addEventListener("phx:page-loading-start", (_info) => topbar.show(1000));
 window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
-// Initialize PostHog
-posthog.init(document.currentScript.dataset.phKey, {
-  api_host: "https://ph.zoonk.com",
-  person_profiles: "always",
-});
+if (process.env.NODE_ENV !== "development") {
+  // Initialize PostHog
+  posthog.init(document.currentScript.dataset.phKey, {
+    api_host: "https://ph.zoonk.com",
+    person_profiles: "always",
+  });
 
-// Identify the user
-posthog.identify(document.currentScript.dataset.userId);
+  // Identify the user
+  posthog.identify(document.currentScript.dataset.userId);
 
-// Capture page views with PostHog
-window.addEventListener("phx:navigate", ({ detail: { href } }) =>
-  posthog.capture("$pageview", {
-    $current_url: href,
-  }),
-);
+  // Capture page views with PostHog
+  window.addEventListener("phx:navigate", ({ detail: { href } }) =>
+    posthog.capture("$pageview", {
+      $current_url: href,
+    }),
+  );
+}
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
