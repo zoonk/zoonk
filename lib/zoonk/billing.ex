841d467b6b947bb87ccc0def0a42936e689ded24
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-05-08 15:39:50 -0300

    Billing: Initialize billing module

---
+++ lib/zoonk/billing.ex
@@ -0,0 +1,8 @@
+defmodule Zoonk.Billing do
+  @moduledoc """
+  Handles billing operations for the application.
+
+  This context provides functions for managing subscriptions, processing payments,
+  and integrating with payment providers such as Stripe.
+  """
+end
