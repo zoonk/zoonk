6f88f67e2d779c49872c35262f27a258c3e636af
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-07-12 13:56:16 -0300

    UserAuthorization: Remove admin checks and tests

--- lib/zoonk_web/user_authorization.ex
+++ lib/zoonk_web/user_authorization.ex
@@ -9,11 +9,8 @@ defmodule ZoonkWeb.UserAuthorization do
   use Gettext, backend: Zoonk.Gettext
 
   alias Zoonk.Accounts.User
-  alias Zoonk.Helpers
   alias Zoonk.Scope
   alias ZoonkWeb.PermissionError
-
-  @admin_contexts [:editor, :org]
 
   @doc """
   LiveView hooks to check organization membership and admin permissions.
@@ -25,14 +22,9 @@ defmodule ZoonkWeb.UserAuthorization do
       2. OR the organization is public (`:app` or `:creator` kind)
       If not, raises a PermissionError.
 
-    * `:ensure_org_admin` - Verifies that the user has an admin role
-      in the organization when accessing paths that start with a value from `@admin_paths`.
-      This check applies to all organization kinds.
-
   ## Examples
 
       on_mount {ZoonkWeb.UserAuthorization, :ensure_org_member}
-      on_mount {ZoonkWeb.UserAuthorization, :ensure_org_admin}
   """
   def on_mount(:ensure_org_member, _params, _session, socket)
       when is_nil(socket.assigns.scope) or is_nil(socket.assigns.scope.user) do
@@ -47,31 +39,7 @@ defmodule ZoonkWeb.UserAuthorization do
     end
   end
 
-  def on_mount(:ensure_org_admin, _params, _session, socket) do
-    context = Helpers.get_context_from_module(socket.view)
-    on_mount_admin(socket, admin_path?: admin_context?(context), org_admin?: org_admin?(socket.assigns.scope))
-  end
-
-  defp on_mount_admin(socket, admin_path?: true, org_admin?: true), do: {:cont, socket}
-
-  defp on_mount_admin(_socket, admin_path?: true, org_admin?: false) do
-    raise PermissionError, code: :require_org_admin
-  end
-
-  defp on_mount_admin(socket, _opts), do: {:cont, socket}
-
   defp org_member?(%Scope{user: %User{confirmed_at: nil}}), do: false
   defp org_member?(%Scope{org_member: nil}), do: false
   defp org_member?(_scope), do: true
-
-  defp org_admin?(%Scope{user: %User{confirmed_at: nil}}), do: false
-  defp org_admin?(%Scope{org_member: nil}), do: false
-  defp org_admin?(%Scope{org_member: org_member}) when org_member.role == :admin, do: true
-  defp org_admin?(_scope), do: false
-
-  defp admin_context?(nil), do: false
-
-  defp admin_context?(context) do
-    Enum.member?(@admin_contexts, context)
-  end
 end
