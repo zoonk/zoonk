a4511b820b1411d8cdbde901ebc411c7f38052c8
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-08-15 21:08:34 -0300

    Evals: Update prompt with new course suggestions

--- lib/zoonk/ai/evals/suggest_courses_eval.ex
+++ lib/zoonk/ai/evals/suggest_courses_eval.ex
@@ -75,7 +75,243 @@ defmodule Zoonk.AI.Evals.SuggestCoursesEval do
 
   @impl EvalCase
   def prompt do
-    []
+    [
+      %{
+        language: "pt",
+        country: "",
+        input: "physics and chemistry",
+        expectations:
+          """
+          - suggestions should include both "Física" and "Química"
+          - should NOT include "Física e Química" as a single title
+          - may include broader or similar topics
+          - all titles and descriptions in Portuguese
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "en",
+        country: "",
+        input: "intro to chemistry",
+        expectations:
+          """
+          - suggestions should include "Chemistry" (without "Intro")
+          - do NOT include "Intro to Chemistry"
+          - may include broader or similar topics
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "en",
+        country: "BR",
+        input: "direito do trabalho",
+        expectations:
+          """
+          - suggestions should include "Brazilian Labor Law" (in English but with Brazilian jurisdiction)
+          - optionally, may include other law suggestions in the Brazilian jurisdiction
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "pt",
+        country: "BR",
+        input: "us constitutional law",
+        expectations:
+          """
+          - suggestions should include "Direito Constitucional dos EUA" (or equivalent title in Portuguese)
+          - optionally, may include other law suggestions in the US jurisdiction
+          - all titles and descriptions in Portuguese
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "pt",
+        country: "BR",
+        input: "international human rights",
+        expectations:
+          """
+          - suggestions should include "Direitos Humanos" without a jurisdiction
+          - optionally, may include other similar global law courses or law courses under Brazilian jurisdiction
+          - all titles and descriptions in Portuguese
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "en",
+        country: "DE",
+        input: "gdpr",
+        expectations:
+          """
+          - suggestions should include "GDPR"
+          - it could include similar suggestions like "EU Data Protection Law" and/or other data protection laws in the European context
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "pt",
+        country: "",
+        input: "dragon ball",
+        expectations:
+          """
+          - suggestions should include "Dragon Ball"
+          - optionally, may include broader alts like "Animação", "Cultura Pop" or related suggestions
+          - all titles and descriptions in Portuguese
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "en",
+        country: "",
+        input: "beatles",
+        expectations:
+          """
+          - suggestions should include "The Beatles"
+          - optionally, may include broader alts like "Rock", "Music History" or related suggestions
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "fr",
+        country: "",
+        input: "f1",
+        expectations:
+          """
+          - suggestions should include "Formule 1" or "F1"
+          - optionally, may include broader alts like "Sports", "Auto Racing" or related suggestions
+          - all titles and descriptions in French
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        language: "pt",
+        country: "",
+        input: "quero aprender ingles e espanhol",
+        expectations:
+          """
+          - suggestions should include either "Inglês" or "Espanhol" or both
+          - should NOT include any other suggestions
+          - all titles and descriptions in Portuguese
+          - `is_language_course` should be true
+          """ <> shared_expectations()
+      },
+      %{
+        language: "en",
+        country: "",
+        input: "jlpt n2",
+        expectations:
+          """
+          - suggestions should include "JLPT" without the "N2" level suffix
+          - optionally, may include "Japanese"
+          - should NOT include any other suggestions
+          - all titles and descriptions in English
+          - `is_language_course` should be true
+          """ <> shared_expectations()
+      },
+      %{
+        language: "en",
+        country: "GB",
+        input: "california employment law",
+        expectations:
+          """
+          - suggestions should include "California Employment Law"
+          - optionally, may include other California-related laws or regulations
+          - should NOT include a generic "Employment Law" suggestion without a jurisdiction
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        input: "quero passar no IELTS Academic",
+        language: "pt",
+        country: "",
+        expectations:
+          """
+          - suggestions should include "IELTS", strip "Academic" suffix
+          - optionally, may include "English"
+          - should NOT include any other suggestions
+          - all titles and descriptions in Portuguese
+          - `is_language_course` should be true
+          """ <> shared_expectations()
+      },
+      %{
+        input: "how computers work?",
+        language: "en",
+        country: "MX",
+        expectations:
+          """
+          - suggestions should include broad caninocals like (but not limited to) "Computer Science", "Computer Architecture", "Operating Systems"
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        input: "quero aprender a programar sites e apps",
+        language: "pt",
+        country: "US",
+        expectations:
+          """
+          - suggestions should include single topics such as "Desenvolvimento Web", "Desenvolvimento Mobile" (but not limited to those, similar ones are fine)
+          - should NOT include joined titles like "Sites e Apps"
+          - all titles and descriptions in Portuguese
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        input: "constitutional law basics",
+        country: "GB",
+        language: "en",
+        expectations:
+          """
+          - suggestions should include "UK Constitutional Law"
+          - may include other law courses in the UK jurisdiction
+          - should NOT include law courses without a jurisdiction
+          - should NOT use the other "basics" or similar terms
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        input: "periodic table",
+        country: "US",
+        language: "pt",
+        expectations:
+          """
+          - suggestions should include "Tabela Periódica"
+          - may include other chemistry-related topics and broad suggestions like "Química"
+          - all titles and descriptions in Portuguese
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        input: "matrix",
+        country: "FR",
+        language: "en",
+        expectations:
+          """
+          - suggestions should include "Matrix" and "The Matrix" (the movie)
+          - may include broader mathematical suggestions like "Linear Algebra"
+          - may include broader film-related suggestions like "Film Studies"
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      },
+      %{
+        input: "pyhsics",
+        country: "AR",
+        language: "en",
+        expectations:
+          """
+          - suggestions should include "Physics"
+          - may include other physics-related topics
+          - all titles and descriptions in English
+          - `is_language_course` should be false
+          """ <> shared_expectations()
+      }
+    ]
   end
 
   defp shared_expectations do
