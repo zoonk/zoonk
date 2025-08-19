157d79912473f71f79600d5552103c0a075cc5c8
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-08-19 14:22:18 -0300

    Assets: Update topbar color

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,7 +54,10 @@ const liveSocket = new LiveSocket("/live", Socket, {
 });
 
 // Show progress bar on live navigation and form submits
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
+topbar.config({
+  barColors: { 0: "#155dfc" },
+  shadowColor: "rgba(0, 0, 0, .3)",
+});
 window.addEventListener("phx:page-loading-start", (_info) => topbar.show(1000));
 window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
 
