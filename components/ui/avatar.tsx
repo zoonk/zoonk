60ce80f2295f5cefc4866dd7d79aca47075c0803
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-09-24 17:19:34 -0300

    UI: Add avatar

---
+++ components/ui/avatar.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { Fallback, Image, Root } from "@radix-ui/react-avatar";
+import type * as React from "react";
+
+import { cn } from "@/lib/utils";
+
+function Avatar({ className, ...props }: React.ComponentProps<typeof Root>) {
+  return (
+    <Root
+      data-slot="avatar"
+      className={cn(
+        "relative flex size-8 shrink-0 overflow-hidden rounded-full",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+function AvatarImage({
+  className,
+  ...props
+}: React.ComponentProps<typeof Image>) {
+  return (
+    <Image
+      data-slot="avatar-image"
+      className={cn("aspect-square size-full", className)}
+      {...props}
+    />
+  );
+}
+
+function AvatarFallback({
+  className,
+  ...props
+}: React.ComponentProps<typeof Fallback>) {
+  return (
+    <Fallback
+      data-slot="avatar-fallback"
+      className={cn(
+        "flex size-full items-center justify-center rounded-full bg-muted",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { Avatar, AvatarImage, AvatarFallback };
