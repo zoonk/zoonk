f35a6499a9371f621e3286147da4a7cff88c2d70
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-09-23 21:09:58 -0300

    Revert "Middleware: Add temporary IP restriction"
    This reverts commit fcc951aee4d0142a95203e0036a48781c9aeaca7.

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,8 @@
-import { type NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "@/i18n/routing";
-import { isIpAllowed } from "@/middleware/ip";
 
 export function middleware(request: NextRequest) {
-  // Temporary, remove this before going to production
-  // When removing this, also update the matcher in the config below
-  if (!isIpAllowed(request) && process.env.VERCEL_ENV) {
-    return NextResponse.redirect("https://zoonk.com");
-  }
-
   const handleI18nRouting = createMiddleware(routing);
   const response = handleI18nRouting(request);
 
