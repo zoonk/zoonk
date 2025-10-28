78dda554ec5e9c1105bbef14d2e03a4cc6c71b06
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-10-28 16:46:47 -0300

    refactor: enhance documentation for useRemovePortal hook

--- packages/ui/src/hooks/use-remove-portal.ts
+++ packages/ui/src/hooks/use-remove-portal.ts
@@ -1,5 +1,21 @@
 import { useLayoutEffect } from "react";
 
+/**
+ * This is a workaround for this issue:
+ * https://github.com/facebook/react/issues/35000
+ *
+ * On Next.js 16, they're using `Activity` to cache routes.
+ * However, `Activity` doesn't hide portals, making them visible on the new route.
+ *
+ * See these links for more context:
+ *
+ * - https://github.com/vercel/next.js/issues/85390
+ * - https://github.com/vercel/next.js/pull/84923
+ *
+ * We can remove this hook once this is fixed on React or Next.js adds a separate
+ * flag for routing caching. Right now, we can't disable it without also disabling
+ * `cacheComponents`.
+ */
 export function useRemovePortal(selector = "[role='dialog']") {
   useLayoutEffect(
     () => () => {
