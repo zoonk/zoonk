923a01199e74be3d7e2c80df313f6707061267ab
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-11-15 16:02:34 -0300

    Config: Update image remote patterns

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -1,11 +1,21 @@
 import type { NextConfig } from "next";
 
+const CACHE_IMAGE_DAYS = 30;
+
 const nextConfig: NextConfig = {
   cacheComponents: true,
   experimental: {
     authInterrupts: true,
     turbopackFileSystemCacheForDev: true,
     typedEnv: true,
+  },
+  images: {
+    minimumCacheTTL: 60 * 60 * 24 * CACHE_IMAGE_DAYS,
+    remotePatterns: [
+      new URL("https://to3kaoi21m60hzgu.public.blob.vercel-storage.com/**"),
+      new URL("https://*.googleusercontent.com/**"),
+      new URL("https://*.githubusercontent.com/**"),
+    ],
   },
   reactCompiler: true,
   transpilePackages: ["@zoonk/auth", "@zoonk/api", "@zoonk/db", "@zoonk/ui"],
