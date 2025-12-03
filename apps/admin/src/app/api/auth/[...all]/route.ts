92187b9dd4058b64904939193d06ce0376d30b2c
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-03 19:04:02 -0300

    remove auth routes

--- apps/admin/src/app/api/auth/[...all]/route.ts
+++
@@ -1,4 +0,0 @@
-import { auth } from "@zoonk/auth";
-import { toNextJsHandler } from "better-auth/next-js";
-
-export const { POST, GET } = toNextJsHandler(auth);
--- apps/main/src/app/api/auth/[...all]/route.ts
+++
@@ -1,4 +0,0 @@
-import { auth } from "@zoonk/auth";
-import { toNextJsHandler } from "@zoonk/auth/next";
-
-export const { POST, GET } = toNextJsHandler(auth);
