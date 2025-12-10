ac4a3498a5cc032a9282bfab498e9fb1fdadf1f2
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-10 20:24:53 -0300

    add org page back

---
+++ apps/editor/src/app/[locale]/(private)/[orgSlug]/layout.tsx
@@ -0,0 +1,25 @@
+import {
+  getOrganizationId,
+  hasCoursePermission,
+} from "@zoonk/core/organizations";
+import { notFound, unauthorized } from "next/navigation";
+
+export default async function OrgHomeLayout({
+  children,
+  params,
+}: LayoutProps<"/[locale]/[orgSlug]">) {
+  const { orgSlug } = await params;
+  const { data: organizationId } = await getOrganizationId(orgSlug);
+
+  if (!organizationId) {
+    return notFound();
+  }
+
+  const canViewPage = await hasCoursePermission(organizationId, "update");
+
+  if (!canViewPage) {
+    return unauthorized();
+  }
+
+  return <>{children}</>;
+}
