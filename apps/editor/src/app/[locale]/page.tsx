7470fd80ecd434b5d61dc324770d3f466b759513
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-10 17:14:35 -0300

    add header

--- apps/editor/src/app/[locale]/page.tsx
+++ apps/editor/src/app/[locale]/page.tsx
@@ -1,16 +1,32 @@
 "use cache";
 
-import { setRequestLocale } from "next-intl/server";
+import {
+  Container,
+  ContainerDescription,
+  ContainerHeader,
+  ContainerTitle,
+} from "@zoonk/ui/components/container";
+import { getExtracted, setRequestLocale } from "next-intl/server";
 import { EditorNavbar } from "@/components/navbar";
 
 export default async function HomePage({ params }: PageProps<"/[locale]">) {
   const { locale } = await params;
   setRequestLocale(locale);
 
+  const t = await getExtracted();
+
   return (
     <>
       <EditorNavbar active="home" />
-      {}
+
+      <Container variant="narrow">
+        <ContainerHeader className="text-center">
+          <ContainerTitle>{t("Select an organization")}</ContainerTitle>
+          <ContainerDescription>
+            {t("Choose an organization to manage its courses")}
+          </ContainerDescription>
+        </ContainerHeader>
+      </Container>
     </>
   );
 }
