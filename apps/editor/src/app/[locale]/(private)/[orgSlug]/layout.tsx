a76da0b49a42e993b6504a7b48c884dfe909dc6c
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-11 11:17:10 -0300

    feat(editor): set active organization for automatic redirection

--- apps/editor/src/app/[locale]/(private)/[orgSlug]/layout.tsx
+++ apps/editor/src/app/[locale]/(private)/[orgSlug]/layout.tsx
@@ -1,7 +1,9 @@
+import { auth } from "@zoonk/auth";
 import {
   getOrganizationBySlug,
   hasCoursePermission,
 } from "@zoonk/core/organizations";
+import { headers } from "next/headers";
 import { notFound, unauthorized } from "next/navigation";
 import { Suspense } from "react";
 import { EditorNavbar } from "./navbar";
@@ -25,6 +27,14 @@ async function LayoutPermissions({
   if (!canViewPage) {
     return unauthorized();
   }
+
+  // We're setting the current org as the active one,
+  // so we can automatically redirect users to this org
+  // next time they visit the editor
+  await auth.api.setActiveOrganization({
+    body: { organizationSlug: orgSlug },
+    headers: await headers(),
+  });
 
   return <div>{children}</div>;
 }
