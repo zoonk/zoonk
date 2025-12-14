b5c00c75324679febd4338bb1f40ba5657d5cf2d
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-14 10:33:38 -0300

    add placeholder note for course page

--- apps/editor/src/app/[locale]/(private)/[orgSlug]/c/[lang]/[courseSlug]/page.tsx
+++ apps/editor/src/app/[locale]/(private)/[orgSlug]/c/[lang]/[courseSlug]/page.tsx
@@ -1,29 +1,20 @@
 import {
   Container,
-  ContainerBody,
   ContainerHeader,
   ContainerTitle,
 } from "@zoonk/ui/components/container";
-import type { Metadata } from "next";
-import { setRequestLocale } from "next-intl/server";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return { title: "Course" };
-}
-
+// this page is just a placeholder for now
 export default async function CoursePage({
   params,
 }: PageProps<"/[locale]/[orgSlug]/c/[lang]/[courseSlug]">) {
-  const { locale, courseSlug } = await params;
-  setRequestLocale(locale);
+  const { courseSlug } = await params;
 
   return (
     <Container variant="narrow">
       <ContainerHeader>
         <ContainerTitle>{courseSlug}</ContainerTitle>
       </ContainerHeader>
-
-      <ContainerBody>{}</ContainerBody>
     </Container>
   );
 }
