0921ce2f24ed27713885fd20bc8c9ae7eb4f4071
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-15 13:45:41 -0300

    update table

--- packages/ui/src/components/table.tsx
+++ packages/ui/src/components/table.tsx
@@ -68,7 +68,7 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
   return (
     <th
       className={cn(
-        "h-10 whitespace-nowrap px-2 text-left align-middle font-medium text-foreground [&:has([role=checkbox])]:pr-0 *:[[role=checkbox]]:translate-y-0.5",
+        "h-12 whitespace-nowrap px-3 text-left align-middle font-medium text-foreground [&:has([role=checkbox])]:pr-0",
         className,
       )}
       data-slot="table-head"
@@ -81,7 +81,7 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
   return (
     <td
       className={cn(
-        "whitespace-nowrap p-2 align-middle [&:has([role=checkbox])]:pr-0 *:[[role=checkbox]]:translate-y-0.5",
+        "whitespace-nowrap p-3 align-middle [&:has([role=checkbox])]:pr-0",
         className,
       )}
       data-slot="table-cell"
