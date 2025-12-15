78a6f53b18db9eb79e174d55a0d21a5490a0163d
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-15 17:05:05 -0300

    update otp bg

--- packages/ui/src/components/input-otp.tsx
+++ packages/ui/src/components/input-otp.tsx
@@ -50,7 +50,7 @@ function InputOTPSlot({
   return (
     <div
       className={cn(
-        "relative flex size-9 items-center justify-center border-input border-y border-r bg-input/30 text-sm outline-none transition-all first:rounded-l-4xl first:border-l last:rounded-r-4xl aria-invalid:border-destructive data-[active=true]:z-10 data-[active=true]:border-ring data-[active=true]:ring-[3px] data-[active=true]:ring-ring/50 data-[active=true]:aria-invalid:border-destructive data-[active=true]:aria-invalid:ring-destructive/20 dark:data-[active=true]:aria-invalid:ring-destructive/40",
+        "relative flex size-9 items-center justify-center border-input border-y border-r bg-background text-sm outline-none transition-all first:rounded-l-4xl first:border-l last:rounded-r-4xl aria-invalid:border-destructive data-[active=true]:z-10 data-[active=true]:border-ring data-[active=true]:ring-[3px] data-[active=true]:ring-ring/50 data-[active=true]:aria-invalid:border-destructive data-[active=true]:aria-invalid:ring-destructive/20 dark:data-[active=true]:aria-invalid:ring-destructive/40",
         className,
       )}
       data-active={isActive}
