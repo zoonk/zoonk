4078f0b72f98aadc65e438202abf55d9c26b4e11
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-19 14:05:26 -0300

    try as-needed

--- apps/editor/src/i18n/routing.ts
+++ apps/editor/src/i18n/routing.ts
@@ -3,6 +3,6 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   defaultLocale: "en",
-  localePrefix: "always",
+  localePrefix: "as-needed",
   locales: SUPPORTED_LOCALES,
 });
