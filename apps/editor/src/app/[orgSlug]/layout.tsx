0ad62e632010ea1b92a5733e9fad473f53340c5d
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-28 14:31:31 -0300

    fix(editor): set active org only if user has permissions

--- apps/editor/src/app/[orgSlug]/layout.tsx
+++ apps/editor/src/app/[orgSlug]/layout.tsx
@@ -3,6 +3,7 @@ import { getOrganization } from "@zoonk/core/orgs/get";
 import { hasCoursePermission } from "@zoonk/core/orgs/permissions";
 import { headers } from "next/headers";
 import { notFound, unauthorized } from "next/navigation";
+import { after } from "next/server";
 import { Suspense } from "react";
 import { EditorNavbar } from "@/components/navbar";
 
@@ -12,19 +13,10 @@ async function LayoutPermissions({
   params,
 }: LayoutProps<"/[orgSlug]">) {
   const { orgSlug } = await params;
+
   const [org, canViewPage] = await Promise.all([
     getOrganization(orgSlug),
-    hasCoursePermission({
-      orgSlug,
-      permission: "update",
-    }),
-    // We're setting the current org as the active one,
-    // so we can automatically redirect users to this org
-    // next time they visit the editor
-    auth.api.setActiveOrganization({
-      body: { organizationSlug: orgSlug },
-      headers: await headers(),
-    }),
+    hasCoursePermission({ orgSlug, permission: "update" }),
   ]);
 
   if (!org.data) {
@@ -34,6 +26,17 @@ async function LayoutPermissions({
   if (!canViewPage) {
     return unauthorized();
   }
+
+  // We're setting the current org as the active one,
+  // so we can automatically redirect users to this org
+  // next time they visit the editor
+  // run this in the background after the page is rendered
+  after(async () => {
+    await auth.api.setActiveOrganization({
+      body: { organizationSlug: orgSlug },
+      headers: await headers(),
+    });
+  });
 
   return (
     <div>
