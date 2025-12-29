823c038a2cd3632cb7fb6e3b4d3b7164a45abac2
Will Ceolin <4393133+ceolinwill@users.noreply.github.com>
2025-12-28 23:57:05 -0300

    feat(editor): add back navigation (#452)

--- apps/editor/src/app/[orgSlug]/c/[lang]/[courseSlug]/ch/[chapterSlug]/page.tsx
+++ apps/editor/src/app/[orgSlug]/c/[lang]/[courseSlug]/ch/[chapterSlug]/page.tsx
@@ -4,11 +4,16 @@ import { SUPPORT_URL } from "@zoonk/utils/constants";
 import { notFound } from "next/navigation";
 import { getExtracted } from "next-intl/server";
 import { Suspense } from "react";
+import {
+  BackLink,
+  BackLinkSkeleton,
+} from "@/app/[orgSlug]/_components/back-link";
 import { ContentEditor } from "@/app/[orgSlug]/_components/content-editor";
 import { ContentEditorSkeleton } from "@/app/[orgSlug]/_components/content-editor-skeleton";
 import { ItemList } from "@/app/[orgSlug]/_components/item-list";
 import { ItemListSkeleton } from "@/app/[orgSlug]/_components/item-list-skeleton";
 import { getChapter } from "@/data/chapters/get-chapter";
+import { getCourse } from "@/data/courses/get-course";
 import { listChapterLessons } from "@/data/lessons/list-chapter-lessons";
 import {
   updateChapterDescriptionAction,
@@ -17,6 +22,29 @@ import {
 
 type ChapterPageProps =
   PageProps<"/[orgSlug]/c/[lang]/[courseSlug]/ch/[chapterSlug]">;
+
+async function ChapterBackLink({
+  params,
+}: {
+  params: ChapterPageProps["params"];
+}) {
+  const { courseSlug, lang, orgSlug } = await params;
+  const { data: course } = await getCourse({
+    courseSlug,
+    language: lang,
+    orgSlug,
+  });
+
+  if (!course) {
+    return null;
+  }
+
+  return (
+    <BackLink href={`/${orgSlug}/c/${lang}/${courseSlug}`}>
+      {course.title}
+    </BackLink>
+  );
+}
 
 async function ChapterContent({
   params,
@@ -87,16 +115,21 @@ async function LessonList({ params }: { params: ChapterPageProps["params"] }) {
 }
 
 export default async function ChapterPage(props: ChapterPageProps) {
-  const { chapterSlug, lang, orgSlug } = await props.params;
+  const { chapterSlug, courseSlug, lang, orgSlug } = await props.params;
 
   // Preload data in parallel (cached, so child components get the same promise)
   void Promise.all([
+    getCourse({ courseSlug, language: lang, orgSlug }),
     getChapter({ chapterSlug, language: lang, orgSlug }),
     listChapterLessons({ chapterSlug, orgSlug }),
   ]);
 
   return (
     <Container variant="narrow">
+      <Suspense fallback={<BackLinkSkeleton />}>
+        <ChapterBackLink params={props.params} />
+      </Suspense>
+
       <Suspense fallback={<ContentEditorSkeleton />}>
         <ChapterContent params={props.params} />
       </Suspense>
