b777b15d65586211f9c91a7465c5c36d6208509b
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-31 17:50:05 -0300

    use unpooled db url on prisma

--- packages/db/prisma.config.ts
+++ packages/db/prisma.config.ts
@@ -1,15 +1,9 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
-const shadowDatabaseUrl =
-  process.env.NODE_ENV === "production"
-    ? env("DATABASE_URL_UNPOOLED")
-    : undefined;
-
 export default defineConfig({
   datasource: {
-    shadowDatabaseUrl,
-    url: env("DATABASE_URL"),
+    url: env("DATABASE_URL_UNPOOLED"),
   },
   migrations: {
     path: "src/prisma/migrations",
