ad781cddc37201384ae90aa8c422b785a5f0789d
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2025-12-31 13:40:08 -0300

    allow to prefetch items

--- apps/main/src/app/[locale]/(catalog)/courses/course-list-client.tsx
+++ apps/main/src/app/[locale]/(catalog)/courses/course-list-client.tsx
@@ -80,7 +80,6 @@ export function CourseListClient({
             linkComponent={
               <ClientLink
                 href={`/b/${course.organization.slug}/c/${course.slug}`}
-                prefetch={false}
               />
             }
           />
