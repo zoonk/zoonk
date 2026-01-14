659dea5334b2aaf199176b4616b3c57b51c8a246
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2026-01-14 10:14:30 -0300

    infer type from nextconfig

--- packages/next/src/security-headers.ts
+++ packages/next/src/security-headers.ts
@@ -1,6 +1,8 @@
-import type { Header } from "next/dist/lib/load-custom-routes";
+import type { NextConfig } from "next";
 
 const isDev = process.env.NODE_ENV === "development";
+
+type Header = Awaited<ReturnType<NonNullable<NextConfig["headers"]>>>[number];
 
 type CSPOptions = {
   imgSrc?: string[];
