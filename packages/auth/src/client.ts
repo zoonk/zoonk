5cf2b27a51fc34c57a348d27a9caa47a0223bda2
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2026-01-31 15:08:32 -0300

    move basePath config out of config file

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -9,8 +9,11 @@ import {
 } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 import { type auth } from "./auth";
-import { basePath } from "./config";
 import { ac, admin, member, owner } from "./permissions";
+
+// When using Better Auth from apps, we use the default `api/auth` base path.
+// However, in the API, we use a different base path, so this value can be overridden
+export const basePath = process.env.NEXT_PUBLIC_AUTH_BASE_PATH || "/api/auth";
 
 export const authClient = createAuthClient({
   basePath,
