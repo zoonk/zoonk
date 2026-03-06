90b7cfa9957518835de4ee0a1072b7a323a8920f
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2026-03-06 16:24:15 -0300

    Revert "disable cache components from admin"
    This reverts commit 409b93fda69f150812c42b099a93abaeb34ed114.

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -3,6 +3,7 @@ import { type NextConfig } from "next";
 const CACHE_IMAGE_DAYS = 30;
 
 const nextConfig: NextConfig = {
+  cacheComponents: true,
   devIndicators: false,
   experimental: {
     authInterrupts: true,
