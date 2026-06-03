0f169ae7d216962ba0a0857d8cac991f3a903717
ceolinwill <4393133+ceolinwill@users.noreply.github.com>
2026-06-03 12:47:14 -0300

    chore: remove video app

--- apps/video/src/videos/launch/schema.ts
+++
@@ -1,5 +0,0 @@
-import { z } from "zod";
-
-export const launchVideoSchema = z.object({ locale: z.enum(["en", "pt-br"]).default("en") });
-
-export type LaunchVideoProps = z.infer<typeof launchVideoSchema>;
